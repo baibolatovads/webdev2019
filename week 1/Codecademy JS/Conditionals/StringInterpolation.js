d38c8a65232eb2b7003b7c0c429a1caad4aa8a21
Baibolatova Dariya <baibolatovads@gmail.com>
2019-01-28 23:22:36 +0600

    first commit

---
+++ week 1/Codecademy JS/Conditionals/StringInterpolation.js
@@ -0,0 +1,5 @@
+let myName = 'Dariya';
+let myCity = 'Almaty';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
---
+++ week 1/Codecademy JS/Conditionals/properties.js
@@ -0,0 +1,22 @@
+//Arithmetic operations
+console.log(3.5 + 19);
+console.log(2019-1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+//Concat
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+//Propertiess
+console.log('Teaching the world how to code'.length);
+
+//Methods
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
+
+//Build-in Objects
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
