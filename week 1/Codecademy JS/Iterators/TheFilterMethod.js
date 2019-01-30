c11d3cd5051afa0e0201a450624b65807354e100
Baibolatova Dariya <baibolatovads@gmail.com>
2019-01-31 01:09:28 +0600

    one more commit

---
+++ week 1/Codecademy JS/Iterators/TheFilterMethod.js
@@ -0,0 +1,16 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(nums =>{
+  return nums < 250;
+})
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+const longFavoriteWords = favoriteWords.filter(words =>{
+  return words.length > 7;
+})
+
+
+// Call .filter() on favoriteWords below
+
